ded2c4b2490f5a53b98c68056dca2b02a2cf9785
XX Pan <panezion@outlook.com>
2022-08-15 16:11:34 +0800

    init my proj

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const github_path: string = process.env.GITHUB_PATH ?? ''
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`GITHUB_PATH: ${github_path}`)
 
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
