cc5c6a12e810d24ccca0f1b6d722d2426aa24714
XX Pan <panezion@outlook.com>
2022-08-17 13:02:10 +0800

    install grpc

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,53 @@
-import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as fse from 'fs-extra'
-import * as tar from 'tar'
+import {info, getInput} from '@actions/core'
+import {exec} from '@actions/exec'
+import {mkdirP} from '@actions/io'
+import {join} from 'path'
 
-async function run(): Promise<void> {
-  try {
-    let distro: string = core.getInput('distro') ?? 'humble'
-    if (distro.length === 0) {
-      distro = 'humble'
-    }
-    // pwd
-    const workspace: string = process.env.GITHUB_WORKSPACE ?? process.cwd()
-    core.debug(`workspace: ${workspace}`)
-    // iterate over all directories in the workspace/src
-    const dirs: string[] = fs.readdirSync(path.join(workspace, 'src'))
+async function install_grpc(version: string): Promise<void> {
+  info('cloning grpc repo...')
+  await exec('git', [
+    'clone',
+    '--depth',
+    '1',
+    '--recurse-submodules',
+    '--shallow-submodules',
+    '-b',
+    version,
+    'https://github.com/grpc/grpc'
+  ])
+  const extPath = 'grpc'
+  info(`Configuring in ${extPath}`)
+  const buildDir = join(extPath, 'build')
+  await mkdirP(buildDir)
+  await exec(
+    'cmake',
+    [
+      '-DgRPC_INSTALL=ON',
+      '-DgRPC_SSL_PROVIDER=package',
+      '-DgRPC_BUILD_TESTS=OFF',
+      '-DgRPC_BUILD_GRPC_PYTHON_PLUGIN=OFF',
+      '-DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF',
+      '-DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF',
+      '-DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF',
+      '-DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF',
+      '-DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF',
+      '-DgRPC_BUILD_CSHARP_EXT=OFF',
+      '-DgRPC_BUILD_TESTS=OFF',
+      '-DgRPC_BUILD_CODEGEN=OFF',
+      '-DgRPC_BACKWARDS_COMPATIBILITY_MODE=ON',
+      '..'
+    ],
+    {cwd: buildDir}
+  )
+  info(`Compiling in ${buildDir}`)
+  await exec('make', ['-j'], {cwd: buildDir})
 
-    for (const dir of dirs) {
-      // if the directory contains a package.xml file
-
-      const pkg_src_dir: string = path.join(workspace, 'src', dir)
-      const pkg_xml_path: string = path.join(pkg_src_dir, 'package.xml')
-
-      core.debug(`Processing ${dir}, package.xml full path: ${pkg_xml_path}`)
-
-      if (fs.existsSync(pkg_xml_path)) {
-        const pkg_name: string = dir
-        core.debug(`Found package.xml at ${pkg_xml_path}`)
-
-        // create a branch called ros2/humble
-        const branch = `ros2/${distro}/${pkg_name}`
-        core.debug(`branch name: ${branch}`)
-        // mkdir branch
-
-        // create a directory for the package(/tmp/${branch}), and copy the files in dir to the directory
-
-        const pkg_dir: string = path.join('/tmp', branch)
-        if (fs.existsSync(pkg_dir)) {
-          fse.removeSync(pkg_dir)
-        }
-
-        fs.mkdirSync(pkg_dir, {recursive: true})
-        core.debug(`pkg dir: ${pkg_dir}`)
-        core.debug(`src dir: ${pkg_src_dir}`)
-        // if proj_dir exists, delete it
-        const file_name = path.join(pkg_dir, `${pkg_name}.tar.gz`)
-
-        tar
-          .c({gzip: true, cwd: pkg_src_dir}, ['.'])
-          .pipe(fs.createWriteStream(file_name))
-      }
-    }
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  await exec('make install', [], {cwd: buildDir})
 }
 
+async function run(): Promise<void> {
+  const version: string = getInput('grpc-version', {required: true})
+  info(`grpc version: ${version}`)
+  install_grpc(version)
+}
 run()
